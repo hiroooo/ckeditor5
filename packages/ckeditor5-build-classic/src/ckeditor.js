d99186f69c8783fe91951fa0f0149b215e750d66
鈴木浩章 <hroaki.suzuki@gmail.com>
2021-01-12 17:26:18 +0900

    fix

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -113,10 +113,6 @@ ClassicEditor.defaultConfig = {
       'myBlockStyle:waku',
       'myBlockStyle:kokuban',
     ],
-    styles: [
-      'waku',
-      'kokuban'
-    ]
   },
 	// This value must be kept in sync with the language defined in webpack.config.js.
 	language: 'ja'
